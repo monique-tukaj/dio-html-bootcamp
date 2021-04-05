e12aef1b13be97650c0448ea4d0f273b79ec818f
Monique Tukaj <59707536+monique-tukaj@users.noreply.github.com>
2021-04-05 21:37:05 +0200

    Update repetition-structure.py

--- repetition-structure.py
+++ repetition-structure.py
@@ -1,3 +1,6 @@
+#Programming Logic class - Prepare the 9x table from 1 until 10.
+#The while loop statement was used to solve this challenge, knowledge acquired from Python programming language.
+
 number = 0
 print("=" * 19)
 while number <= 10:
